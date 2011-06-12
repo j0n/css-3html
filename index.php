000ba6884ecf6912e4b0f4fc7f1bf974d576735f
Jon <jon@c-9962e555.219-8-64736c16.cust.bredbandsbolaget.se>
2011-06-12 20:55:17 +0200

    anim working a great load of css thoug

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
   </head>
   <body>
     <div class='first'>
-      <?php for($i = 0;$i<10;$i++){ ?>
-        <?php for($j = 0;$j<10;$j++){ ?>
-            <div id='square_<?php echo $i . '_'.$j; ?>' href='#square_<?php echo $i . '_'.$j; ?>'>
+      <?php for($i = 0;$i<20;$i++){ ?>
+        <?php for($j = 0;$j<20;$j++){ ?>
+            <div id='square_<?php echo $i . '_'.$j; ?>' class='row_<?php echo $i;?> col_<?php echo $j?>' href='#square_<?php echo $i . '_'.$j; ?>'>
         <?php } ?>
       <?php } ?>
-      <div id='character'></div>
-      <?php for($i = 0;$i<10;$i++){ ?>
-        <?php for($j = 0;$j<10;$j++){ ?>
+      <?php for($i = 1;$i<9;$i++){ ?>
+        <span id='character_<?php echo $i;?>' class='char'></span>
+      <?php } ?>
+      <?php for($i = 0;$i<20;$i++){ ?>
+        <?php for($j = 0;$j<20;$j++){ ?>
             </div>
         <?php } ?>
       <?php } ?>
